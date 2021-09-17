5067c67da2f82ba00de250511ce9b1f3d888bd5e
alaa <alaaalh437@gamil.com>
2021-09-18 00:20:50 +0200

    first day in js

---
+++ js-D1/TaskOne.js
@@ -0,0 +1,22 @@
+// for(var i=0 ; i<=10 ; i++){
+//     if(i == 5){
+//         i = 'alaa';
+//     }
+//     console.log(i)
+// }
+
+
+
+
+// let myArr = ['Alaa', 'Ali' , 'Mohamed','Rehab' , 'Ahmed'];
+// for(let i = 0 ; i < myArr.length ; i++){
+//     console.log(i+1 +'-'+ myArr[i])
+// }
+
+
+
+// var header=document.getElementById('test');
+// console.log(header.getAttribute('attr'))
+// for (let i=0 ; i<header.getAttribute('attr'); i++ ){
+//     console.log(i)
+// }
